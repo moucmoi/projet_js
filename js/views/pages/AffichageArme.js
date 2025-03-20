e715b06466b96d8a06fb2c3cc58f55951af11548
moucmoi <noatoimoi@gmail.com>
2025-03-20 08:16:18 +0100

    commit intermédiaire

--- js/views/pages/AffichageArme.js
+++ js/views/pages/AffichageArme.js
@@ -25,19 +25,5 @@ export default class AffichageArme {
           </a>
         `;
       }
-    }
-
-    renderSupp(arme) {
-        let request = Utils.parseRequestURL();
-        return `
-          <link rel="stylesheet" href='../../../css/PersonnageMenu.css'>
-          <a href='/#/characters/${request.id}/suppression/${arme.id}' class="personnage-card-link" id="personnage-card-link">
-              <section class="personnage-section" id="personnage-section">
-                  <img src="${arme.image}" alt="${arme.name}" class="personnage-img" id="personnage-img">
-                  <h3 class="personnage-name" id="personnage-name">${arme.name}</h3>
-              </section>
-          </a>
-        `;
-      }
-      
 }
+
