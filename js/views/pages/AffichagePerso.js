1481fc9f9adabf5345a125764f797465aa3ae5e3
moucmoi <noatoimoi@gmail.com>
2025-03-23 20:15:55 +0100

    ajout lazy loading

--- js/views/pages/AffichagePerso.js
+++ js/views/pages/AffichagePerso.js
@@ -5,7 +5,7 @@ export default class AffichagePerso {
 
             <a href='/#/characters/${character.id}' class="personnage-card-link" id="personnage-card-link">
                 <section class="personnage-section" id="personnage-section">
-                    <img src="${character.image}" alt="${character.name}" class="personnage-img" id="personnage-img">
+                    <img loading="lazy"  src="${character.image}" alt="${character.name}" class="personnage-img" id="personnage-img">
                     <h3 class="personnage-name" id="personnage-name">${character.name}</h3>
                     <h4 class="personnage-importance" id="personnage-importance">${character.importance}</h4>
                 </section>
