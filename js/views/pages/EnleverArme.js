282c69b448f7b437036f327477944e821588dcbe
moucmoi <noatoimoi@gmail.com>
2025-03-26 17:02:05 +0100

    css ajout

--- js/views/pages/EnleverArme.js
+++ js/views/pages/EnleverArme.js
@@ -1,24 +1,23 @@
-import CharacterProvider from "../../services/CharacterProvider.js";
-import Utils from "../../services/Utils.js";
-export default class EnleverArme{
+export default class EnleverArme {
     async render(){
-        let request = Utils.parseRequestURL();
-        let character=await CharacterProvider.getCharacter(request.id);
-        let view='';
-        try{
-            await CharacterProvider.retirerArme(character,request.id2);
-            view+=`
-            <link href="./../../../css/AjoutSupp.css" rel="stylesheet" />
-            <div id="suppression-arme" class="page-container">
-                <h2>L'arme a bien été supprimée</h2>
-                <div class="confirm-buttons">
-                    <button onclick=window.location.href='/#/characters/${request.id}'>Retour</button>
-                </div>
-            </div>`;
-        }
-        catch{
-            view+=`<h2>La suppression ne s'est pas faite</h2>`
-        }
-        return view;
+      let request = Utils.parseRequestURL();
+      let character = await CharacterProvider.getCharacter(request.id);
+      let view = '';
+      try {
+        await CharacterProvider.retirerArme(character, request.id2);
+        view += `
+          <link href="./../../../css/AjoutSupp.css" rel="stylesheet" />
+          <div id="enlever-arme" class="page-container">
+            <h2>L'arme a bien été supprimée</h2>
+            <div class="confirm-buttons">
+              <button id="retour-enlever-arme" onclick="window.location.href='/#/characters/${request.id}'">Retour</button>
+            </div>
+          </div>
+        `;
+      } catch {
+        view += `<h2>La suppression ne s'est pas faite</h2>`;
+      }
+      return view;
     }
-}
+  }
+  
--- js/views/pages/NouvelleArme.js
+++ js/views/pages/NouvelleArme.js
@@ -7,29 +7,30 @@ export default class NouvelleArme {
             <link href="./../../../css/AjoutSupp.css" rel="stylesheet" />
             <div id="nouvelle-arme" class="page-container">
                 <h2>Créer une nouvelle arme</h2>
-
+                
                 <label for="name">Nom :</label>
                 <input type="text" id="name" required />
-
+                
                 <label for="force">Bonus de Force :</label>
                 <input type="number" id="force" required />
-
+                
                 <label for="agilite">Bonus d'Agilité :</label>
                 <input type="number" id="agilite" required />
-
+                
                 <label for="intelligence">Bonus d'Intelligence :</label>
                 <input type="number" id="intelligence" required />
-
+                
                 <label for="endurance">Bonus d'Endurance :</label>
                 <input type="number" id="endurance" required />
-
+                
                 <div class="page-buttons">
-                    <button id="creerArme">Créer</button>
+                <button id="creerArme">Créer</button>
                 </div>
-
+                
                 <p id="message"></p>
             </div>
         `;
+
     }
 
     async afterRender() {
