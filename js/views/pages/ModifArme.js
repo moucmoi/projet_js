1073ec26d974dfbb9697bd4a2b57128f8fe69e74
marin <marin.chesneau31@gmail.com>
2025-04-02 14:41:00 +0200

    refactoring modifArme

--- js/views/pages/ModifArme.js
+++ js/views/pages/ModifArme.js
@@ -1,4 +1,5 @@
 import ArmeProvider from "../../services/ArmeProvider.js";
+import { modifArmeController } from "../../controllers/modifArmeController.js";
 import Utils from "../../services/Utils.js";
 
 export default class ModifArme {
@@ -30,76 +31,19 @@ export default class ModifArme {
                 <input type="file" id="image" accept="image/png" />
 
                 <div class="page-buttons">
-                <button id="annuler" onclick="location.href = '/#/armes/${arme.id}';">Annuler</button>
-                <button id="modifArme">Modifier</button>
+                    <button id="annuler" onclick="location.href = '/#/armes/${arme.id}';">Annuler</button>
+                    <button id="modifArme">Modifier</button>
                 </div>
 
                 <p id="message"></p>
             </div>
         `;
-
     }
 
     async afterRender() {
         let request = Utils.parseRequestURL();
         let armeID = request.id;
-        let arme = await ArmeProvider.getArme(armeID);
-    
-        document.getElementById("modifArme").addEventListener("click", async () => {
-            let name = document.getElementById("name").value.trim();
-            let force = parseInt(document.getElementById("force").value.trim());
-            let agilite = parseInt(document.getElementById("agilite").value.trim());
-            let intelligence = parseInt(document.getElementById("intelligence").value.trim());
-            let endurance = parseInt(document.getElementById("endurance").value.trim());
-            let imageFile = document.getElementById("image").files[0];
-            let image = arme.image;
-    
-            if (name === "") {
-              document.getElementById("message").textContent = "Le nom doit être rempli.";
-              return;
-            }
         
-            if (!Number.isFinite(force) || !Number.isFinite(agilite) || !Number.isFinite(intelligence) || !Number.isFinite(endurance)) {
-                document.getElementById("message").textContent = "Les caractéristiques doivent être des nombres valides.";
-                return;
-            }
-
-            if (imageFile) {
-                try {
-                  const reader = new FileReader();
-                  reader.readAsDataURL(imageFile);
-      
-                  await new Promise((resolve) => {
-                    reader.onload = () => {
-                      image = reader.result;
-                      resolve();
-                    };
-                  });
-                } catch (error) {
-                  document.getElementById("message").textContent =
-                    "Erreur lors du chargement de l'image.";
-                  return;
-                }
-              }
-    
-            let armeData = {
-                name,
-                effects: {
-                    force,
-                    agilite,
-                    intelligence,
-                    endurance
-                },
-                image
-            };
-    
-            let success = await ArmeProvider.updateArme(armeID, armeData);
-    
-            if (success) {
-                window.location.assign(`/#/armes/${armeID}`);
-            } else {
-                document.getElementById("message").textContent = "Erreur de modification.";
-            }
-        });
+        modifArmeController(armeID);
     }
 }
